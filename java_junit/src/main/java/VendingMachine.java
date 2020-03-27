a6fdb304b89cfa6b427f028c2d8b3567624da7ed
csduser <a@a>
2020-03-27 15:50:58 +0900

    working

--- java_junit/src/main/java/VendingMachine.java
+++ java_junit/src/main/java/VendingMachine.java
@@ -1,8 +1,24 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VendingMachine {
 
     int totalDeposit = 0;
+    private final Map<String, Integer> drink = new HashMap<>();
+    private final Map<String, Integer> stock = new HashMap<>();
+
+    public VendingMachine() {
+        registerMenu("water",100);
+        registerMenu("tea",120);
+        registerMenu("richWater",120);
+        registerMenu("superRichWater",150);
+    }
+
+    private void registerMenu(String name, int price) {
+        drink.put(name,price);
+        stock.put(name,0);
+    }
 
     public void receive(int money) {
         if (Arrays.asList(10, 50, 100, 500, 1000).contains(money)) {
@@ -11,8 +27,9 @@ public class VendingMachine {
     }
 
     public String provide(String order) {
-        if (totalDeposit >= getPrice(order)) {
+        if (totalDeposit >= getPrice(order) && stock.get(order) > 0) {
             totalDeposit -= getPrice(order);
+            stock.put(order, stock.get(order) - 1);
             return order;
         } else {
             return null;
@@ -20,8 +37,7 @@ public class VendingMachine {
     }
 
     private int getPrice(String order) {
-        return order.equals("water") ? 100 : 120;
-
+        return drink.get(order);
     }
 
     public int displayTotalDeposit() {
@@ -32,5 +48,13 @@ public class VendingMachine {
         int change = totalDeposit;
         totalDeposit = 0;
         return change;
+    }
+
+    public void stock(String drink) {
+        stock.put(drink, stock.get(drink) + 1);
+    }
+
+    public int getStock(String drink) {
+        return stock.get(drink);
     }
 }
--- java_junit/src/test/java/VendingMachineTest.java
+++ java_junit/src/test/java/VendingMachineTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,18 +13,23 @@ public class VendingMachineTest {
 
     static Stream<Arguments> buyPatterns() {
         return Stream.of(
-                arguments(Arrays.asList(100), "water", "water"),
-                arguments(Arrays.asList(100, 10, 10), "water", "water"),
-                arguments(Arrays.asList(100, 10, 10), "tea", "tea"),
-                arguments(Arrays.asList(100), "tea", null)
+                arguments(Arrays.asList("water"), Arrays.asList(100), "water", "water"),
+                arguments(Arrays.asList("water"), Arrays.asList(100, 10, 10), "water", "water"),
+                arguments(Arrays.asList("richWater"), Arrays.asList(100, 10, 10), "richWater", "richWater"),
+                arguments(Arrays.asList("tea"), Arrays.asList(100, 10, 10), "tea", "tea"),
+                arguments(Arrays.asList("superRichWater"), Arrays.asList(100, 50), "superRichWater", "superRichWater"),
+                arguments(Arrays.asList("tea"), Arrays.asList(100), "tea", null),
+                arguments(Arrays.asList(), Arrays.asList(1000), "tea", null),
+                arguments(Arrays.asList("tea"), Arrays.asList(1000), "water", null)
         );
     }
 
     @ParameterizedTest
     @MethodSource("buyPatterns")
-    public void buy(List<Integer> depositList, String order, String expected) {
+    public void buy(List<String> stockList, List<Integer> depositList, String order, String expected) {
         //given
         VendingMachine vendingMachine = new VendingMachine();
+        stockList.forEach(vendingMachine::stock);
         depositList.forEach(vendingMachine::receive);
         //when
         String drink = vendingMachine.provide(order);
@@ -55,24 +59,80 @@ public class VendingMachineTest {
         //when
         depositList.forEach(vendingMachine::receive);
         //then
+        assertEquals(expected, vendingMachine.totalDeposit);
         assertEquals(expected, vendingMachine.displayTotalDeposit());
     }
 
     static Stream<Arguments> changePatterns() {
         return Stream.of(
-                arguments(Arrays.asList(500), "water", 400),
-                arguments(Arrays.asList(500), "tea", 380)
+                arguments(Arrays.asList("water"), Arrays.asList(500), "water", 400),
+                arguments(Arrays.asList("tea"), Arrays.asList(500), "tea", 380),
+                arguments(Arrays.asList("richWater"), Arrays.asList(500), "richWater", 380),
+                arguments(Arrays.asList("superRichWater"), Arrays.asList(500), "superRichWater", 350),
+                arguments(Arrays.asList("superRichWater"), Arrays.asList(100), "superRichWater", 100)
         );
     }
 
     @ParameterizedTest
     @MethodSource("changePatterns")
-    public void returnChange(List<Integer> depositList, String order, int expected) {
+    public void returnChange(List<String> stockList, List<Integer> depositList, String order, int expected) {
+        //given
         VendingMachine vendingMachine = new VendingMachine();
+        stockList.forEach(vendingMachine::stock);
         depositList.forEach(vendingMachine::receive);
         vendingMachine.provide(order);
-
-        assertEquals(expected, vendingMachine.returnChange());
+        //when
+        int actual = vendingMachine.returnChange();
+        //then
+        assertEquals(expected, actual);
         assertEquals(0, vendingMachine.displayTotalDeposit());
     }
+
+    static Stream<Arguments> stockPatterns() {
+        return Stream.of(
+                arguments(Arrays.asList("water"), "water", 1),
+                arguments(Arrays.asList("tea"), "tea", 1),
+                arguments(Arrays.asList("richWater"), "richWater", 1),
+                arguments(Arrays.asList("superRichWater"), "superRichWater", 1),
+                arguments(Arrays.asList("water", "water"), "water", 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("stockPatterns")
+    public void stock(List<String> drinkList, String drink, int expected) {
+        //given
+        VendingMachine vendingMachine = new VendingMachine();
+
+        //when
+        drinkList.forEach(vendingMachine::stock);
+
+        //then
+        assertEquals(expected, vendingMachine.getStock(drink));
+    }
+
+    static Stream<Arguments> leftStockPatterns() {
+        return Stream.of(
+                arguments(Arrays.asList("water"), Arrays.asList(100), "water", 0),
+                arguments(Arrays.asList("water", "tea"), Arrays.asList(100, 10, 10), "tea", 0),
+                arguments(Arrays.asList("tea"), Arrays.asList(100, 10, 10), "water", 0),
+                arguments(Arrays.asList(), Arrays.asList(100, 10, 10), "water", 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("leftStockPatterns")
+    public void leftStock(List<String> drinkList, List<Integer> paymentList, String order, int expected) {
+        //given
+        VendingMachine vendingMachine = new VendingMachine();
+        drinkList.forEach(vendingMachine::stock);
+        paymentList.forEach(vendingMachine::receive);
+
+        //when
+        vendingMachine.provide(order);
+
+        //then
+        assertEquals(expected, vendingMachine.getStock(order));
+    }
+
 }
