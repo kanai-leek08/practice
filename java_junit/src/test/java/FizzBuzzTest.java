21b72e4eb6927d69e72cc3ef91207c74b34391a0
d.kanai <d.kanai@pastoraldog.com>
2020-05-07 19:49:03 +0900

    working

--- java_junit/src/test/java/FizzBuzzTest.java
+++ java_junit/src/test/java/FizzBuzzTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
     @Test
     public void testTest() {
-        assertEquals(1, 1);
+        assertEquals(2, 2);
     }
 }
 
