9be816a22299255e18a86288d461242da46e24c8
pasumaru <d.kanai@pastoraldog.com>
2020-05-08 14:41:48 +0900

    empty message

--- java_junit/src/test/java/FizzBuzzTest.java
+++ java_junit/src/test/java/FizzBuzzTest.java
@@ -1,9 +1,12 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
 
     @Test
     void name() {
+        assertEquals(1,1);
 
     }
 }
