2a7bf37742ac51fad8e1444c66a81b98d4c66b69
pasumaru <d.kanai@pastoraldog.com>
2020-05-08 15:58:24 +0900

    working

--- java_junit/src/test/java/VendingMachineTest.java
+++ java_junit/src/test/java/VendingMachineTest.java
@@ -3,7 +3,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VendingMachineTest {
 
     public void testInsert(){
+        new VendingMachine();
         assertEquals(1,1);
+    }
+
+    class VendingMachine{
+
     }
     
 }
