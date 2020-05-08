4cab98d2376db94e14a4041fe7d8d937371b649e
dev1611058hu <keita.suzuki05@g.softbank.co.jp>
2020-05-08 16:14:44 +0900

    working

--- java_junit/src/test/java/VendingMachineTest.java
+++ java_junit/src/test/java/VendingMachineTest.java
@@ -44,7 +44,13 @@ public class VendingMachineTest {
     }
 
     public enum Yen {
-        YEN10, YEN50, YEN100, YEN500, YEN1000
+        YEN10(10), YEN50(50), YEN100(100), YEN500(500), YEN1000(1000);
+
+        private int value;
+
+        Yen(int value) {
+            this.value = value;
+        }
     }
 
 }
