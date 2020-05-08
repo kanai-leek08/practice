2093dee77dfa2b997143311ec327850608e2affc
pasumaru <d.kanai@pastoraldog.com>
2020-05-08 11:05:10 +0900

    working

--- java_junit/src/test/java/FizzBuzzTest.java
+++ java_junit/src/test/java/FizzBuzzTest.java
@@ -3,15 +3,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+
     @Test
     public void testInput1got1() {
-        String actual = new FizzBuzz().execute();
+        String actual = new FizzBuzz().execute(1);
         assertEquals("1", actual);
     }
 
+    @Test
+    public void testInput2got2() {
+        String actual = new FizzBuzz().execute(2);
+        assertEquals("2", actual);
+    }
+
+
+
     class FizzBuzz {
 
-        String execute() {
+        String execute(int number) {
+            if(number == 2){
+                return "2";
+            }
             return "1";
         }
 
