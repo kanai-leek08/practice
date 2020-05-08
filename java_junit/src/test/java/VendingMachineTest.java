b57851b4cd4d2b0673a1dc0f966f3bc0e6e11eda
pasumaru <d.kanai@pastoraldog.com>
2020-05-08 16:16:10 +0900

    working

--- java_junit/src/test/java/VendingMachineTest.java
+++ java_junit/src/test/java/VendingMachineTest.java
@@ -43,14 +43,6 @@ public class VendingMachineTest {
 
     }
 
-    public enum Yen {
-        YEN10(10), YEN50(50), YEN100(100), YEN500(500), YEN1000(1000);
-
-        private int value;
-
-        Yen(int value) {
-            this.value = value;
-        }
-    }
+ 
 
 }
